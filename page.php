c787fa3282dc9d8489becc6f255599ae2bde2ca9
Waldo Jaquith <waldo@jaquith.org>
2025-01-19 19:16:01 -0500

    Provide a complete page title

--- page.php
+++ page.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo get_the_title() . ' | ' . wp_get_document_title(); ?></title>
     <?php wp_site_icon(); ?>
     <?php wp_head(); ?>
 </head>
