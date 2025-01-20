8338d7b68cb91a67d22b5dc7e70f142ee5f18dde
Waldo Jaquith <waldo@jaquith.org>
2025-01-19 19:15:52 -0500

    Add favicons

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo wp_get_document_title(); ?></title>
+    <?php wp_site_icon(); ?>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title(); ?></title>
+    <?php wp_site_icon(); ?>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
