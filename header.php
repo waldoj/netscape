72cecf5c5887d8c3d27129f81b83fcc2b15e96b8
Waldo Jaquith <waldo@jaquith.org>
2025-01-19 19:10:38 -0500

    Remove incorrect header material
    Turns out this isn't what header.php is for!

--- header.php
+++ header.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
 <header>
     <div class="container">
         <h1><a class="blog-name" href="/"><?php bloginfo('name'); ?></a></h1>
