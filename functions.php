1ea974618096581cb1bb752141d4491da4c806b3
Waldo Jaquith <waldo@jaquith.org>
2025-02-02 16:26:54 -0500

    Add RSS & favicon support

--- functions.php
+++ functions.php
@@ -15,6 +15,12 @@ add_action('after_setup_theme', function() {
     add_image_size('featured-large', 1200, 800, true);
     add_image_size('featured-medium', 800, 600, true);
     
+    // Add RSS feed support
+    add_theme_support('automatic-feed-links');
+    
+    // Add site icon support
+    add_theme_support('site-icon');
+    
     // Add title tag support
     add_theme_support('title-tag');
 });
