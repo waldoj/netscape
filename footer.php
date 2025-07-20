59080233b7af4d92fceae3c879e5eedd86096246
Waldo Jaquith <waldo@jaquith.org>
2025-07-19 23:14:44 -0400

    Add Netscape badge

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
         <hr />
         <p>Copyright <?php echo date('Y'); ?> <?php bloginfo('name'); ?>, but it’s possible
         I’ll let you reproduce this somewhere if you <a href="/contact/">ask nicely</a>.</p>
+        <img src="/blog/wp-content/themes/netscape/assets/images/netscape.gif" alt="Netscape Now!" width="88" height="31" />
     </div>
 </footer>
 
