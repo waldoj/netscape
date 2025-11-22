b81c3287e2e35639ddf0d0cf596bf2d29599b134
Waldo Jaquith <waldo@jaquith.org>
2025-11-22 13:36:02 -0500

    Fix misplaced tag
    Thanks to VSCode for "helping"

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <hr />
         <p>Copyright <?php echo date('Y'); ?> <?php bloginfo('name'); ?>, but it’s possible
         I’ll let you reproduce this somewhere if you <a href="/contact/">ask nicely</a>.</p>
-        <a href="https://www.netscape-communications.com/download/"></a><img src="/blog/wp-content/themes/netscape/assets/images/netscape.gif" alt="Netscape Now!" width="88" height="31" /></div></footer>a>
+        <a href="https://www.netscape-communications.com/download/"><img src="/blog/wp-content/themes/netscape/assets/images/netscape.gif" alt="Netscape Now!" width="88" height="31" /></div></footer></a>
     </div>
 </footer>
 
