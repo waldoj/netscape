a0c8119e35a5b8eaae2056fddf448e058ccebd43
Waldo Jaquith <waldo@jaquith.org>
2026-05-23 17:10:52 -0400

    Fix shortlink formatting
    Format the "shortlinks" category differently, styled to be different than standard blog entries. Intended for, well, short links to other things.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div class="container">
         <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
-                <div class="post">
+                <div <?php post_class('post'); ?>>
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="post-meta">
                         <span class="date"><?php the_date(); ?></span>
